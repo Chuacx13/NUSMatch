6cdb25325bdc7de86601ca7290c89fa833e7a70f
Chuacx13 <chuachongxuan@gmail.com>
2023-05-28 00:10:14 +0800

    login-authenticate

--- script/home.js
+++ script/home.js
@@ -1,9 +1,9 @@
-const loginButton = document.querySelector('.js-login-button');
+const loginButton = document.querySelector('.js-login-form-button');
 loginButton.addEventListener('click', () => goToLogin());
 
 const goToLogin = () => {location.href = 'login.html'};
 
-const registerButton = document.querySelector('.js-register-button');
+const registerButton = document.querySelector('.js-register-form-button');
 registerButton.addEventListener('click', () => goToRegister());
 
 const goToRegister = () => {location.href = 'register.html'};
--- script/login.js
+++ script/login.js
@@ -1,4 +1,39 @@
 const registerLink = document.querySelector('.js-register-link');
 registerLink.addEventListener('click', () => goToRegister());
 
+const loginButton = document.querySelector('.js-login-button');
+loginButton.addEventListener('click', () => loginToAccount());
+
 const goToRegister = () => {location.href = 'register.html'};
+
+const initialUserList = [
+    { username: 'user',
+    email: 'user@test.com',
+    password: 'testpassword' }
+];
+
+localStorage.setItem('userList', JSON.stringify(initialUserList));
+
+const userList = JSON.parse(localStorage.getItem('userList'));
+
+function checkDetails(username, password) {
+    for (let i = 0; i < userList.length; i++) {
+        if ((userList[i].username === username || userList[i].email == username) && userList[i].password === password) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+function loginToAccount() {
+    const inputUsername = document.querySelector('.js-username-input').value;
+    const inputPassword = document.querySelector('.js-password-input').value;
+    if (inputUsername.trim() === '' || inputPassword.trim() === '') {
+        return;
+    }
+    if (checkDetails(inputUsername, inputPassword)) {
+        document.querySelector('.js-message').innerHTML = 'Success';
+    } else {
+        document.querySelector('.js-message').innerHTML = 'Failed';
+    }
+}
