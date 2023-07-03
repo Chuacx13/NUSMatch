a70d10a0bbada957a4b24f2c32d32f6b0357e0ae
Chuacx13 <chuachongxuan@gmail.com>
2023-07-04 01:14:09 +0800

    backend host

--- server/src/routes/usersRoute.js
+++ server/src/routes/usersRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_ADMIN_SDK_KEY_PATH);
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_KEY_PATH);
 const userRouter = express.Router();
 
 admin.initializeApp({
